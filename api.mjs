96e3fdffa72b6e606f62fb85eb4cde51d3424a9e
AdarshRai3 <git config --global user.email>
2023-09-21 23:00:28 +0530

    commit5

--- api.mjs
+++ api.mjs
@@ -2,7 +2,8 @@
 // npm install crypto-js
 
 // Then, you can use the MD5 function from crypto-js as follows:
-import MD5 from "crypto-js/md5.js";
+import MD5 from "./node_modules/crypto-js/md5.js";
+
 
 // Your public and private keys
 var public_key = "801ffb179c43e62422d9c6ac3f1203f0";
